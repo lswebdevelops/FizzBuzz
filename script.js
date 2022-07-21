f1a6936f65b8fd117f485f87eff40164863483fb
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-07-20 21:43:57 -0300

    Script and style ready

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-console.log("hi");
-// testing git
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: ", 20));
+
+
+let text= "";
+for(let i = 1; i <= answer; i++){
+    if (i % 3 === 0 && i % 5 === 0 ){
+        text += ">  "+ "FizzBuzz" + "<br>"; 
+    }
+    else if(i % 5 === 0){
+        text += ">  "+ "Buzz" + "<br>"; 
+
+    }else if(i % 3 === 0){
+        text += ">  "+ "Fizz" + "<br>"; 
+               
+    }else {
+        text += ">  "+ i + "<br>";
+    }
+
+
+ 
+}
+document.querySelector('p').innerHTML = text;
+
